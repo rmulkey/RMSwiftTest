0ddf56b13dd69ced0f7595941ed3ae84aa11d749
Rodrigo Mulkey <rmulkey@gmail.com>
2014-06-04 23:11:46 -0400

    adjusting constraints, adding controller boilerplate code;

--- RMSwiftTest/RMSwiftTest/RSSViewController.swift
+++ RMSwiftTest/RMSwiftTest/RSSViewController.swift
@@ -6,4 +6,19 @@
 //  Copyright (c) 2014 Rodrigo Mulkey. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RSSViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
