84760c1c43869fbb7756bb9be64f463de5dee102
Rodrigo Mulkey <rmulkey@gmail.com>
2014-06-05 00:29:24 -0400

    creating menu (tableview)

--- RMSwiftTest/RMSwiftTest/AppDelegate.swift
+++ RMSwiftTest/RMSwiftTest/AppDelegate.swift
@@ -15,9 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        let rootVC: ViewController = ViewController(nibName: nil, bundle: nil)
+        self.window!.rootViewController = rootVC
+        self.window!.makeKeyAndVisible()
         return true
     }
+
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ RMSwiftTest/RMSwiftTest/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  RMSwiftTest
+//
+//  Created by Rodrigo Mulkey on 6/4/14.
+//  Copyright (c) 2014 Rodrigo Mulkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
+    
+    var tableView: UITableView!
+    var redditThreads = NSString[]()
+    var redditThreadsData: NSArray = NSArray()
+    var data: NSMutableData!
+    
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView = UITableView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height), style: UITableViewStyle.Plain)
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "redditThread")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.view.addSubview(self.tableView)
+        self.refreshRedditData()
+    }
+    
+    func refreshRedditData(){
+        var url: NSURL = NSURL(string: "http://reddit.com/r/swift.json")
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate:self, startImmediately: false)
+        connection.start()
+    }
+    
+    // connection delegate
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        // Recieved a new request, clear out the data object
+        self.data = NSMutableData()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        // Append the recieved chunk of data to our data object
+        self.data.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        var err: NSError
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        
+        if(jsonResult["data"].count > 0){
+            var data: NSDictionary = jsonResult["data"] as NSDictionary
+            if(data["children"].count > 0){
+                self.redditThreadsData = data["children"] as NSArray
+                //                for child: Dictionary<String, AnyObject> in self.redditThreadsData {
+                for var i = 0; i < self.redditThreadsData.count; ++i {
+                    var child: Dictionary<String, AnyObject> = self.redditThreadsData[i] as (Dictionary<String, AnyObject>)
+                    var threadInfo: NSDictionary = child["data"] as NSDictionary
+                    var title: String = threadInfo["title"] as String
+                    self.redditThreads.append(title)
+                }
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int{
+        return self.redditThreads.count
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("redditThread", forIndexPath: indexPath) as UITableViewCell
+        cell.text = self.redditThreads[indexPath.row]
+        return cell
+        
+    }
+}
+
